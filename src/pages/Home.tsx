979d38d6e2148a2c69581be72b44d787d245c5f8
Ville Paananen <villepaananen@outlook.com>
2022-02-25 11:40:15 +0200

    initial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+
+import { Storage } from "@ionic/storage";
 
 const Home: React.FC = () => {
+  const store = new Storage();
+
   return (
     <IonPage>
       <IonHeader>
