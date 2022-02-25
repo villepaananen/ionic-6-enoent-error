0f7bf0f48652f6273dde1029b1b8d0fdb2f0ea56
Ville Paananen <villepaananen@outlook.com>
2022-02-25 11:34:47 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-6-testing',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
